4e520da6888636ce3a8dc1ab6fad5db0b6d17dd1
Amruth <ammumandappa@gmail.com>
2024-08-04 11:51:05 +0530

    .

--- first-angular-app/src/app/app.component.ts
+++ first-angular-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppNavbar } from './navbar/navbar.components';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppNavbar],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: './app.component.css'
 })
 export class AppComponent {
   title = 'first-angular-app';
--- first-angular-app/src/app/navbar/navbar.components.ts
+++ first-angular-app/src/app/navbar/navbar.components.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
-Component({
-  selector: 'app-navbar',
-  standalone: true,
+@Component({
+  selector: 'app-navbar', // Make sure this matches
+  standalone:true,
   template: '<h1>Navbar component</h1>',
-});
-
+})
 export class AppNavbar {}
