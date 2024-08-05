f9326248952444c4266a0375cb8a4267e640e159
Amruth <ammumandappa@gmail.com>
2024-08-05 22:49:42 +0530

    if else

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-isLoggedIn: boolean = true;
+isLoggedIn: boolean = false;
 userName:string = 'john doe'}
