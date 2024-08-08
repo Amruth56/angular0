bb1357eadbdf209a45dd2521f419440afae1f53a
Amruth <ammumandappa@gmail.com>
2024-08-08 22:58:13 +0530

    ng template outlet

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FormsModule, NgForm } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { NgIf, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf],
+  imports: [RouterOutlet, NgIf, NgTemplateOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
