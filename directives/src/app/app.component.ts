abfa5bcba08674bfc906ab1468654cdbf1f3a47a
Amruth <ammumandappa@gmail.com>
2024-08-09 09:59:34 +0530

    .

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FormsModule, NgForm } from '@angular/forms';
-import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
+import { NgClass, NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, NgTemplateOutlet, NgFor],
+  imports: [RouterOutlet, NgIf, NgTemplateOutlet, NgFor, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
@@ -25,4 +25,6 @@ export class AppComponent {
   // }
 
   users:Array<string> = ['john', 'sam', 'smith', 'raj']
+
+  
 }
