07cb8fe90f8db2bd0115c241d06530bed1f8f452
Amruth <ammumandappa@gmail.com>
2024-08-09 08:34:12 +0530

    ngFor

--- directives/src/app/app.component.ts
+++ directives/src/app/app.component.ts
@@ -2,25 +2,27 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { FormsModule, NgForm } from '@angular/forms';
-import { NgIf, NgTemplateOutlet } from '@angular/common';
+import { NgFor, NgIf, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, NgTemplateOutlet],
+  imports: [RouterOutlet, NgIf, NgTemplateOutlet, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  isLoggedIn: boolean = true;
-  userName: string = 'john doe';
-  loginCount: number = 0;
+  // isLoggedIn: boolean = true;
+  // userName: string = 'john doe';
+  // loginCount: number = 0;
 
-  userRole: string = 'Admin';
+  // userRole: string = 'Admin';
 
-  countLogInAttempts() {
-    this.loginCount++;
-    if (this.loginCount > 3) {
-    }
-  }
+  // countLogInAttempts() {
+  //   this.loginCount++;
+  //   if (this.loginCount > 3) {
+  //   }
+  // }
+
+  users:Array<string> = ['john', 'sam', 'smith', 'raj']
 }
