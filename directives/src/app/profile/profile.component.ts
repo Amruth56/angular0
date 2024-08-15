08f687e28437b3795382c9b2cbe35b2cdc312e68
Amruth <ammumandappa@gmail.com>
2024-08-15 22:18:22 +0530

    .

--- directives/src/app/profile/profile.component.ts
+++ directives/src/app/profile/profile.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnChanges, Input, OnInit, DoCheck } from '@angular/core';
+import {
+  Component,
+  OnChanges,
+  Input,
+  OnInit,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+} from '@angular/core';
 
 @Component({
   selector: 'app-profile',
@@ -7,29 +15,38 @@ import { Component, OnChanges, Input, OnInit, DoCheck } from '@angular/core';
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css',
 })
-export class ProfileComponent implements OnChanges, OnInit, DoCheck {
+export class ProfileComponent
+  implements OnChanges, OnInit, DoCheck, AfterContentInit, AfterContentChecked
+{
   @Input() pUserName: string = '';
   counter: number = 0;
 
-  constructor(){
-    console.log('constructor method triggered')
-    console.log(this.pUserName)
+  constructor() {
+    console.log('constructor method triggered');
+    console.log(this.pUserName);
   }
 
-ngOnInit(){
-  console.log('NgOnInit hook triggered')
-  console.log(this.pUserName)
-
-}
+  ngOnInit() {
+    console.log('NgOnInit hook triggered');
+    console.log(this.pUserName);
+  }
 
   ngOnChanges() {
     console.log('onChanges triggered');
   }
 
-  ngDoCheck(){
-    console.log('ngDoCheck triggered')
+  ngDoCheck() {
+    console.log('ngDoCheck triggered');
   }
-  incrementCounter(){
+  incrementCounter() {
     this.counter++;
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit triggered');
+  }
+
+  ngAfterContentChecked() {
+    console.log('AfterContentChecked triggered ');
   }
 }
