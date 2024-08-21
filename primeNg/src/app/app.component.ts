9f8fa79ecebfa334d3ec83c68fc2bf92c91771f1
Amruth <ammumandappa@gmail.com>
2024-08-21 15:26:58 +0530

    .

--- primeNg/src/app/app.component.ts
+++ primeNg/src/app/app.component.ts
@@ -3,16 +3,33 @@ import { RouterOutlet } from '@angular/router';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenubarModule, OnInit],
+  imports: [RouterOutlet, MenubarModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit{
-  title = 'primeNg';
+export class AppComponent implements OnInit {
   items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-home',
+        routerLink: ['/'],
+      },
+      {
+        label: 'User List',
+        icon: 'pi pi-users',
+        routerLink: ['/userList'],
+      },
+      {
+        label: 'Add User',
+        icon: 'pi pi-user-plus',
+        routerLink: ['/newUser'],
+      },
+    ];
+  }
 }
--- primeNg/src/app/app.routes.ts
+++ primeNg/src/app/app.routes.ts
@@ -2,13 +2,13 @@ import { Routes } from '@angular/router';
 import { UserComponent } from './components/user/user.component';
 import { AddUserComponent } from './components/add-user/add-user.component';
 
-
 export const routes: Routes = [
-    { 
-        path: 'userList', 
-        component: UserComponent, 
-    }, {
-        path :'newUser',
-        component: AddUserComponent
-    }
-]
+  { 
+    path: 'userList', 
+    component: UserComponent 
+  },
+  { 
+    path: 'newUser', 
+    component: AddUserComponent 
+  }
+];
